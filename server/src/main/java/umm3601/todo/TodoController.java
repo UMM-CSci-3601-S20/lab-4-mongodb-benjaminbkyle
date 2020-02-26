81c9040c2f9f29ef31c6fb97be96075dd8c16622
scvstackkk <scv.stack@gmail.com>
2020-02-26 15:15:10 -0600

    #17 - Add todo directory, #16 Add Todo & TodoController skeletons

---
+++ server/src/main/java/umm3601/todo/TodoController.java
@@ -0,0 +1,30 @@
+package umm3601.todo;
+
+import static com.mongodb.client.model.Filters.and;
+import static com.mongodb.client.model.Filters.eq;
+import static com.mongodb.client.model.Filters.regex;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import com.google.common.collect.ImmutableMap;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Sorts;
+
+import org.bson.Document;
+import org.bson.conversions.Bson;
+import org.bson.types.ObjectId;
+import org.mongojack.JacksonCodecRegistry;
+
+import io.javalin.http.BadRequestResponse;
+import io.javalin.http.Context;
+import io.javalin.http.NotFoundResponse;
+
+public class TodoController {
+
+}
