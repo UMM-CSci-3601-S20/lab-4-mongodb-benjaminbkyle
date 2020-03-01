1a578e48a0ab4c68b824f19749fb33ae89a0e950
scvstackkk <scv.stack@gmail.com>
2020-02-29 21:29:59 -0600

    Write todo body filtering e2e tests. Add html element for todo.body

--- client/e2e/src/todo-list.e2e-spec.ts
+++ client/e2e/src/todo-list.e2e-spec.ts
@@ -16,8 +16,53 @@ describe('Todo list', () => {
   });
 
   // ---------- Filtering Tests ---------- //
+  // ---------- Body Filtering ---------- //
+  it('Check that there is one todo with body provided from DB', async () => {
+    await page.typeInput('todo-body-input', 'Ullamco irure laborum magna dolor non. Anim occaecat adipisicing cillum eu magna in.');
 
-  // ---------- Filtering by Category ---------- //
+    // All of the todos returned should have the body we are filtering by
+    page.getTodoListItems().each( async e => {
+      const body = await e.element(by.className('todo-list-body')).getText();
+      expect(body.toLowerCase()
+      .includes('ullamco irure laborum magna dolor non. anim occaecat adipisicing cillum eu magna in.')).toBe(true);
+    });
+
+  });
+
+  it('Check that all todos returned contain the substring \'tempor\' in body', async () => {
+    await page.typeInput('todo-body-input', 'tempor');
+
+    // All todos should have this body as a substring
+    page.getTodoListItems().each(async e => {
+      const body = await e.element(by.className('todo-list-body')).getText();
+      expect(body.toLowerCase().includes('tempor')).toBe(true);
+    });
+
+  });
+
+  it('Should type \'asddfjkfjksdjfkd\' in the body filter and check that it returned no todos', async () => {
+    await page.typeInput('todo-body-input', 'asddfjkfjksdjfkd');
+
+    // Should be no todos with this body
+    page.getTodoListItems().each(async e => {
+      const body = await e.element(by.className('todo-list-body')).getText();
+      expect(body.toLowerCase().length === 0).toBe(true);
+    });
+  });
+
+  it('Should type empty string in the body filter and check that it returned all todos (no filtering)', async () => {
+    await page.typeInput('todo-body-input', '');
+
+    // Should be all todos with this body
+    page.getTodoListItems().each(async e => {
+      const body = await e.element(by.className('todo-list-body')).getText();
+      expect(body.toLowerCase().length > 0).toBe(true);
+     });
+  });
+
+
+
+  // ---------- Category Filtering ---------- //
   it('Should type video games in the category filter and check that it returned the correct elements', async () => {
     await page.typeInput('todo-category-input', 'video games');
 
@@ -26,6 +71,7 @@ describe('Todo list', () => {
       expect(e.element(by.className('todo-list-category')).getText()).toEqual('video games');
     });
   });
+
   it('Should type asddfjkfjksdjfkd in the category filter and check that it returned no todos', async () => {
     await page.typeInput('todo-category-input', 'asddfjkfjksdjfkd');
 
@@ -34,14 +80,12 @@ describe('Todo list', () => {
       expect(e.element(by.className('todo-list-category')).count().toEqual(0));
     });
   });
+
   it('Should type empty string in the category filter and check that it returned all todos (no filtering)', async () => {
     await page.typeInput('todo-category-input', '');
-    let count = 0;
     // All of the todos returned should have the category we are filtering by
     page.getTodoListItems().each(e => {
-      count++;
+
     });
-    expect(count > 0);
-    //expect(count === 300); - passes when using default "dev" database file w/300 todos. Otherwise, adding will break this test
   });
 });
